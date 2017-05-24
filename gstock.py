9dfbccf01927a920650151d438cd4595ab4ce4a0
Lumsden <qil@mac83207.ornl.gov>
2017-05-24 12:57:03 -0400

    New version of python file that adds opening data

--- gstock.py
+++ gstock.py
@@ -9,10 +9,12 @@ fig = mpyplot.figure(figsize=(10.0, 3.0))
 h_data = google[:,1]
 l_data = google[:,2]
 c_data = google[:,3]
+o_data = google[:,0]
 
 axes1 = fig.add_subplot(4, 1, 1)
 axes2 = fig.add_subplot(4, 1, 2)
 axes3 = fig.add_subplot(4, 1, 3)
+axes4 = fig.add_subplot(4, 1, 4)
 
 axes1.set_ylabel("Daily High Share Prices")
 axes1.plot(h_data)
@@ -23,7 +25,10 @@ axes2.plot(l_data)
 axes3.set_ylabel("Daily Closing Share Prices")
 axes3.plot(c_data)
 
-fig.tight_layout()
+axes4.set_ylabel("Daily Opening Share Prices")
+axes4.plot(o_data)
+
+fig.tight_layout(pad=0.025,w_pad=0.025,h_pad=0.5)
 
 mpyplot.show()
 
